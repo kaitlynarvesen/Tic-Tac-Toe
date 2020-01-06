2da74fd4c1924c058d105e60337a9985d8e4c62a
Kaitlyn Arvesen <kaitlynarvesen@gmail.com>
2020-01-06 12:25:28 -0600

    added way to choose who goes first and way to deal with ties (full board)

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 import random
 
+#print a board with correct format
 def disp(w, board):
 	for row in board:
 		for elem in row:
@@ -7,27 +8,26 @@ def disp(w, board):
 		w.write('\n')
 	w.write('\n')
 
+#check if user input for row or column is valid (int 1-3)
 def input_check(i):
-	flag=0 #okay
-	n=0
 	try:
 		n=int(i)
 	except:
-		flag=1 #can't convert to int
-	if n<1 or n>3:
-		flag=1 #out of range
-	if flag==0:
+		n=0 #invalid
+	if n==1 or n==2 or n==3:
 		return n
 	else:
 		print('Invalid input.')
 		return input_check(input('Row: '))
 
+#check if a spot is taken or not
 def spot_check(board, row, col):
 	if board[row-1][col-1]=='_':
 		return True #not taken
 	else:
 		return False
 
+#check if the board is in a winning configuration
 def win(board, c):
 	#rows
 	if board[0][0]==c and board[0][1]==c and board[0][2]==c:
@@ -50,7 +50,7 @@ def win(board, c):
 		return True
 	return False
 
-
+#player turn
 def player_move(w, board):
 	print('--Your move--')
 	turn=0
@@ -62,12 +62,14 @@ def player_move(w, board):
 			turn=1
 		else:
 			print("Spot taken.")
+	#check for win
 	if win(board, 'X'):
 		disp(w, board)
 		print('Player wins!')
 		exit()
 	return board
 
+#computer turn
 def computer_move(w, board):
 	print('--Computer move--')
 	turn=0
@@ -77,24 +79,82 @@ def computer_move(w, board):
 		if spot_check(board, row, col): #if spot not taken
 			board[row-1][col-1]='O'
 			turn=1
+	#check for win
 	if win(board, 'O'):
 		disp(w, board)
 		print('Computer wins!')
 		exit()
 	return board
 
+#check if the input for the first move guess is valid (int 1-100)
+def first_check(i):
+	try:
+		n=int(i)
+	except:
+		n=-1 #invalid
+	if n>0 and n<101:
+		return n
+	else:
+		print('Invalid input.')
+		return first_check(input('Choose a number from 1-100 (inclusive): '))
 
+#plays the game
 def solve(r, w):
 	#initialize board
 	board=[['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
-	#display
+
+	#determine if player or computer goes first by guessing a number from 1-100
+	#whoever is closer to a random number from 1-100 goes first
+	ans=random.randint(1, 100)
+	player=first_check(input('\nChoose a number from 1-100 (inclusive): '))
+	computer=random.randint(1, 100)
+	print('\nAnswer: '+str(ans)+', Player Guess: '+str(player)+', Computer Guess: '+str(computer))
+	if abs(ans-player)==abs(ans-computer):
+		print('Tie!')
+		solve()
+	if abs(ans-player)<abs(ans-computer):
+		print('Player goes first!\n')
+		solve_player(r, w, board)
+	else:
+		print('Computer goes first!\n')
+		solve_computer(r, w, board)
+
+#used if player goes first
+def solve_player(r, w, board):
 	disp(w, board)
-	
-	while 1==1:
+
+	player_move(w, board)
+	disp(w, board)
+
+	#play until someone wins or the board is full
+	count=0
+	while count<4:
+		computer_move(w, board)
+		disp(w, board)
+
+		player_move(w, board)
+		disp(w, board)
+
+		count+=1
+	print('Tie!')
+	exit()
+
+
+#used if computer goes first
+def solve_computer(r, w, board):
+	computer_move(w, board)
+	disp(w, board)
+
+	#play until someone wins or the board is full
+	count=0
+	while count<4:
 		player_move(w, board)
 		disp(w, board)
 
 		computer_move(w, board)
 		disp(w, board)
 
+		count+=1
+	print('Tie!')
+	exit()
 
