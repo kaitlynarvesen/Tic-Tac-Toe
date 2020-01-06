d366c5ca4077373fcac5a0bc6e51be557f424f59
Kaitlyn Arvesen <kaitlynarvesen@gmail.com>
2020-01-06 11:36:41 -0600

    added function to check for valid input and if a spot is already taken

--- Game.py
+++ Game.py
@@ -7,18 +7,50 @@ def disp(w, board):
 		w.write('\n')
 	w.write('\n')
 
+def input_check(i):
+	flag=0 #okay
+	n=0
+	try:
+		n=int(i)
+	except:
+		flag=1 #can't convert to int
+	if n<1 or n>3:
+		flag=1 #out of range
+	if flag==0:
+		return n
+	else:
+		print('Invalid input.')
+		return input_check(input('Row: '))
+
+def spot_check(board, row, col):
+	if board[row-1][col-1]=='_':
+		return True #not taken
+	else:
+		return False
+
+
 def player_move(board):
 	print('--Your move--')
-	row=int(input('Row: ')) #add check that it's an int 1-3
-	col=int(input('Column: '))
-	board[row-1][col-1]='X' #add check that spot isn't already taken
+	turn=0
+	while turn==0:
+		row=input_check(input('Row: '))
+		col=input_check(input('Column: '))
+		if spot_check(board, row, col): #if spot not taken
+			board[row-1][col-1]='X'
+			turn=1
+		else:
+			print("Spot taken.")
 	return board
 
 def computer_move(board):
 	print('--Computer move--')
-	row=random.randint(1, 3)
-	col=random.randint(1, 3)
-	board[row-1][col-1]='O'
+	turn=0
+	while turn==0:
+		row=random.randint(1, 3)
+		col=random.randint(1, 3)
+		if spot_check(board, row, col): #if spot not taken
+			board[row-1][col-1]='O'
+			turn=1
 	return board
 
 
@@ -33,6 +65,9 @@ def solve(r, w):
 	disp(w, board)
 
 	computer_move(board)
+	disp(w, board)
+
+	player_move(board)
 	disp(w, board)
 
 	#while win==0:
